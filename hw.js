a83a7d59ab7db92ede72871e3c9e802788456fb1
HAnjula <handapangodageachinthana@gmail.com>
2023-06-10 02:25:05 +0530

    test

--- hw.js
+++ hw.js
@@ -1,2 +1,8 @@
-const date = new Date();
-console.log(date);
+function calculate(birthDate) {
+            birthDate = new Date(birthDate);
+            currentDate = new Date();
+                    let years = (currentDate.getFullYear() - birthDate.getFullYear());
+                    let x=document.getElementById("age")
+        
+            return x.innerHTML='Age is : '+years+'.'
+  }
